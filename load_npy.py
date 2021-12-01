49433c97f9c56289e23b5b2844ffdb6e5f49e3af
qazw7910 <okmijn7980@gmail.com>
2021-12-01 09:32:15 +0800

    just test my parameter

--- load_npy.py
+++ load_npy.py
@@ -10,4 +10,7 @@ if __name__ == '__main__':
     path = Path("data/fall/0000.npy")
     var = np.load(path)
     var = torch.from_numpy(var)
-    print(var.shape)
+    num_keypoint = 25
+    feature_array = np.zeros([45, num_keypoint, 3], np.float32)
+    print(feature_array.shape)
+    print(len(feature_array))
