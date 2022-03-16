bcef93f4b8dc1f1fc09e1e61f36f4027cde26c77
qazw7910 <okmijn7980@gmail.com>
2022-03-16 20:06:00 +0800

    new flow of train

---
+++ train_new.py
@@ -0,0 +1,158 @@
+import torch
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+from keypoint_models.models import *
+from keypoint_models.dataset import BodyKeypointsDataset
+from keypoint_models.transform import *
+from preprocess.BODY_25 import BODY_25
+
+
+def train_model(model, train_loader, val_loader, test_loader, epoch):  # test loader
+    model = model.cuda()
+    # optimizer = optim.SGD(model.parameters(), lr=0.0003, momentum=0, weight_decay=0)
+    optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=0.001)
+
+    criterion = torch.nn.CrossEntropyLoss()  # https://zhuanlan.zhihu.com/p/98785902
+
+    loss_history = {'train': [], 'val': [], 'test': []}
+    acc_history = {'train': [], 'val': [], 'test': []}
+    dataloaders = {'train': train_loader, 'val': val_loader, 'test': test_loader}
+    best_val_acc = 0
+    loss = 0
+    acc = 0
+
+    for i in range(epoch):
+        if i <= 100:
+            optimizer = optim.Adam(model.parameters(), lr=0.0001)
+        elif i >= 200:
+            optimizer = optim.Adam(model.parameters(), lr=0.00005)
+
+        if i == 1 or i % 10 == 0:
+            print(f'epoch: {i}, best_val_acc: {best_val_acc}, loss: {loss}, acc: {acc}')
+
+        for phase in ['train', 'val', 'test']:
+            epoch_loss = 0.0
+            epoch_correct = 0
+            num_data = 0
+
+            # dataset.set_phase(phase)
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            for batch in dataloaders[phase]:
+                X = batch['feature']
+                y = batch['label']
+
+                X = X.cuda()
+                y = y.cuda()
+
+                optimizer.zero_grad()
+                with torch.set_grad_enabled(phase == 'train'):
+
+                    output = model(X)
+                    loss = criterion(output, y)
+
+                    if phase == 'train':
+                        loss.backward()
+                        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5)
+                        optimizer.step()
+
+                pred = output.argmax(dim=1)
+
+                num_data += len(y)
+                epoch_correct += (pred == y).sum().item()
+                epoch_loss += loss.item() * len(y)
+
+            loss = epoch_loss / num_data
+            acc = epoch_correct / num_data * 100
+            loss_history[phase].append(loss)
+            acc_history[phase].append(acc)
+
+            if phase in ['val', 'test']:
+                best_val_acc = max(best_val_acc, acc)
+
+    for phase in ['train', 'val', 'test']:
+        plt.plot(range(len(loss_history[phase])), loss_history[phase], label=phase)
+    plt.legend()
+    plt.xlabel('# of iterations')
+    plt.ylabel('loss')
+    plt.show()
+
+    plt.clf()
+    for phase in ['train', 'val', 'test']:
+        plt.plot(range(len(acc_history[phase])), acc_history[phase], label=phase)
+    plt.legend()
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+    return best_val_acc
+
+
+keypoints = [BODY_25.Nose, BODY_25.Neck, BODY_25.RShoulder, BODY_25.RElbow, BODY_25.RWrist,
+             BODY_25.LShoulder, BODY_25.LElbow, BODY_25.LWrist, BODY_25.MidHip,
+             BODY_25.RHip, BODY_25.RKnee, BODY_25.RAnkle, BODY_25.LHip, BODY_25.LKnee,
+             BODY_25.LAnkle]
+keypoints = [point.value for point in keypoints]
+
+num_feature = 2 * len(keypoints)  # because(X,Y) so feature *2
+
+torch.manual_seed(0)
+
+train_transforms = transforms.Compose([Centralize(),
+                                       Scale(0.7, 1.3),
+                                       RandomShift(-0.3, 0.3, -0.3, 0.3)
+                                       ])  # for lstm
+
+val_transforms = transforms.Compose([])
+
+test_transforms = transforms.Compose([])
+
+time_steps = 15
+train_dataset = BodyKeypointsDataset(keypoints, root_dir='data_classification/train_dataset', timesteps=time_steps,
+                                     transforms={'train': train_transforms}, pad_by_last=True)
+val_dataset = BodyKeypointsDataset(keypoints, root_dir='data_classification/val_dataset', timesteps=time_steps,
+                                   transforms={'val': val_transforms}, pad_by_last=True)
+test_dataset = BodyKeypointsDataset(keypoints, root_dir='data_classification/test_dataset', timesteps=time_steps,
+                                    transforms={'val': test_transforms}, pad_by_last=True)
+
+cnt_class = [0, 0]  # TODO cnt_class = [0, 0, 0, 0]
+num_classes = len(cnt_class)
+for data in train_dataset:
+    cnt_class[data['label']] += 1
+
+class_weight = [1 / cnt for cnt in cnt_class]
+
+train_size = len(train_dataset)
+data_weight = [0] * train_size
+for i, data in enumerate(train_dataset):
+    label = data['label']
+    data_weight[i] = class_weight[label]
+
+sampler = torch.utils.data.WeightedRandomSampler(weights=data_weight, num_samples=len(train_dataset),
+                                                 replacement=True)  # https://blog.csdn.net/tyfwin/article/details/108435756
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, sampler=sampler)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=16)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16)
+
+# model = Conv1D(signal_length=time_steps, num_classes=num_classes)
+model = Keypoint_LSTM(
+    input_size=30,
+    hidden_size=64,
+    num_layers=1,
+    num_classes=2
+)
+res = train_model(model, train_loader, val_loader, test_loader, 300)
+print('best_val_acc:', res)
+
+torch.save(model.state_dict(), 'pt_model/fall_10_fps.pth')
+
+'''model = Conv1D(signal_length=time_steps, num_classes=num_classes)
+model.load_state_dict(torch.load('pt_model/fall.pth'))
+model.eval()'''
