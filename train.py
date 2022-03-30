f1155343574f72006e864d523c55b17f7ddf14d3
qazw7910 <okmijn7980@gmail.com>
2022-03-30 20:22:46 +0800

    new conf_mat plot

--- train.py
+++ train.py
@@ -8,30 +8,42 @@ from keypoint_models.models import *
 from keypoint_models.dataset import BodyKeypointsDataset
 from keypoint_models.transform import *
 from preprocess.BODY_25 import BODY_25
+import seaborn as sns
+import numpy as np
 
 
-def train_model(model, train_loader, val_loader, test_loader, epoch):  #test loader
+def train_model(model, train_loader, val_loader, test_loader, epoch):  # test loader
     model = model.cuda()
     # optimizer = optim.SGD(model.parameters(), lr=0.0003, momentum=0, weight_decay=0)
-    optimizer = optim.Adam(model.parameters(), lr=0.00015, weight_decay=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.0005, weight_decay=0.001)
 
-    criterion = torch.nn.CrossEntropyLoss()
+    loss_fn = torch.nn.CrossEntropyLoss()  # https://zhuanlan.zhihu.com/p/98785902
+    phases = ['train', 'val', 'test']
 
-    loss_history = {'train': [], 'val': [], 'test': []}
-    acc_history = {'train': [], 'val': [], 'test': []}
-    dataloaders = {'train': train_loader, 'val': val_loader , 'test': test_loader}
+    loss_history = {phase: [] for phase in phases}
+    acc_history = {phase: [] for phase in phases}
+    dataloaders = {'train': train_loader, 'val': val_loader, 'test': test_loader}
+    conf_mat_each_phase = {phase: None for phase in phases}
     best_val_acc = 0
+    loss = 0
+    acc = 0
 
     for i in range(epoch):
-        if i == 500:
+        if i <= 100:
             optimizer = optim.Adam(model.parameters(), lr=0.0001)
-        elif i == 800:
+        elif i >= 200:
             optimizer = optim.Adam(model.parameters(), lr=0.00005)
+        elif i >= 300:
+            optimizer = optim.Adam(model.parameters(), lr=0.000025)
 
-        for phase in ['train', 'val', 'test']:
+        if i == 1 or i % 10 == 0:
+            print(f'epoch: {i}, best_val_acc: {best_val_acc}, loss: {loss}, acc: {acc}')
+
+        for phase in phases:
             epoch_loss = 0.0
             epoch_correct = 0
             num_data = 0
+            conf_mat = np.zeros((2, 2), np.int32)
 
             dataset.set_phase(phase)
             if phase == 'train':
@@ -50,7 +62,7 @@ def train_model(model, train_loader, val_loader, test_loader, epoch):  #test loa
                 with torch.set_grad_enabled(phase == 'train'):
 
                     output = model(X)
-                    loss = criterion(output, y)
+                    loss = loss_fn(output, y)
 
                     if phase == 'train':
                         loss.backward()
@@ -59,14 +71,18 @@ def train_model(model, train_loader, val_loader, test_loader, epoch):  #test loa
 
                 pred = output.argmax(dim=1)
 
+                for t, p in zip(y, pred):
+                    conf_mat[t.item(), p.item()] += 1
+
                 num_data += len(y)
                 epoch_correct += (pred == y).sum().item()
                 epoch_loss += loss.item() * len(y)
 
             loss = epoch_loss / num_data
-            acc = epoch_correct / num_data *100
+            acc = ((conf_mat[0, 0] + conf_mat[1, 1]) / conf_mat.sum()).item() * 100
             loss_history[phase].append(loss)
             acc_history[phase].append(acc)
+            conf_mat_each_phase[phase] = conf_mat
 
             if phase in ['val', 'test']:
                 best_val_acc = max(best_val_acc, acc)
@@ -82,9 +98,22 @@ def train_model(model, train_loader, val_loader, test_loader, epoch):  #test loa
     for phase in ['train', 'val', 'test']:
         plt.plot(range(len(acc_history[phase])), acc_history[phase])
     plt.legend(['train', 'validation', 'test'])
-    plt.xlabel('# of iterations')
-    plt.ylabel('accuracy')
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
     plt.show()
+
+    plt.clf()
+    for phase in phases:
+        fig, ax = plt.subplots()
+        im = ax.matshow(conf_mat_each_phase[phase], cmap="Blues")
+        for (x, y), val in np.ndenumerate(conf_mat_each_phase[phase]):
+            ax.text(y, x, '{:2d}'.format(val), ha='center', va='center')
+
+        plt.title(f"confusion matrix in {phase} phase")
+        plt.xlabel('prediction')
+        plt.ylabel('actual')
+        fig.colorbar(im)
+        plt.show()
 
     return best_val_acc
 
@@ -102,24 +131,23 @@ torch.manual_seed(0)
 train_transforms = transforms.Compose([Centralize(),
                                        Scale(0.7, 1.3),
                                        RandomShift(-0.3, 0.3, -0.3, 0.3)
-                                       ]) #for lstm
+                                       ])  # for lstm
 
 val_transforms = transforms.Compose([])
 
 test_transforms = transforms.Compose([])
 
-time_steps = 45
-dataset = BodyKeypointsDataset(keypoints, root_dir='data', timesteps=time_steps, transforms={'train': train_transforms, 'val': val_transforms, 'test':test_transforms},
+time_steps = 20
+dataset = BodyKeypointsDataset(keypoints, root_dir='data', timesteps=time_steps, transforms={'train': train_transforms, 'val': val_transforms, 'test': test_transforms},
                                pad_by_last=True)
 
 # split_lengths = [len(dataset) // 2, len(dataset) - len(dataset) // 2]
-train_size =int(len(dataset) * 0.5)
-val_size =int(len(dataset) * 0.3)
-test_size =int(len(dataset)) - train_size - val_size
-train_dataset, val_dataset ,test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
+train_size = int(len(dataset) * 0.8)
+val_size = int(len(dataset) * 0.1)
+test_size = int(len(dataset)) - train_size - val_size
+train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
 
-
-cnt_class = [0, 0]    #TODO cnt_class = [0, 0, 0, 0]
+cnt_class = [0, 0]  # TODO cnt_class = [0, 0, 0, 0]
 num_classes = len(cnt_class)
 for data in train_dataset:
     cnt_class[data['label']] += 1
@@ -132,13 +160,12 @@ for i, data in enumerate(train_dataset):
     label = data['label']
     data_weight[i] = class_weight[label]
 
-sampler = torch.utils.data.WeightedRandomSampler(weights=data_weight, num_samples=512, replacement=True)    #https://blog.csdn.net/tyfwin/article/details/108435756
-
+sampler = torch.utils.data.WeightedRandomSampler(weights=data_weight, num_samples=len(train_dataset),
+                                                 replacement=True)  # https://blog.csdn.net/tyfwin/article/details/108435756
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, sampler=sampler)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=16)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16)
-
 
 # model = Conv1D(signal_length=time_steps, num_classes=num_classes)
 model = Keypoint_LSTM(
@@ -147,10 +174,10 @@ model = Keypoint_LSTM(
     num_layers=1,
     num_classes=2
 )
-res = train_model(model, train_loader, val_loader, test_loader, 18)
+res = train_model(model, train_loader, val_loader, test_loader, 10)
 print('best_val_acc:', res)
 
-torch.save(model.state_dict(), 'pt_model/fall.pth')
+torch.save(model.state_dict(), f'pt_model/fall_{time_steps}_fps.pth')
 
 '''model = Conv1D(signal_length=time_steps, num_classes=num_classes)
 model.load_state_dict(torch.load('pt_model/fall.pth'))
