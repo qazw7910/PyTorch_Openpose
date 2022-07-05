5b4a65ff6f6c2711a59ef9fb519829499c3a05d8
agx <okmijn7980@gmail.com>
2022-07-05 20:06:57 +0800

    Parameterize model_trt.py

--- model_trt.py
+++ model_trt.py
@@ -1,8 +1,10 @@
+import argparse
 import contextlib
 import shutil
 import time
 from io import BytesIO
 from pathlib import Path
+from typing import Sequence
 
 import onnx
 import torch
@@ -11,12 +13,21 @@ from torch2trt import torch2trt, TRTModule
 from keypoint_models.models import KeypointLSTM
 
 
-def main():
-    # pth_path = Path("trt_model/fall_20_fps.pth")
-    # mode = "trt"
+def main(args: Sequence[str] = None):
+    # setup paths and check existence
+    args = get_arg_parser().parse_args(args)
+    pth_path: Path = args.original_model
+    if not pth_path.is_file():
+        raise FileNotFoundError(pth_path)
 
-    pth_path = Path("pt_model/fall_20_fps.pth")
-    mode = "pytorch"
+    out: Path = args.trt_out
+    if not out.is_dir():
+        if out.exists():
+            raise NotADirectoryError(out)
+        else:
+            raise FileNotFoundError(out)
+
+    mode = args.original_model_mode
 
     with eval_time("load_model"):
         weights = torch.load(pth_path)
@@ -57,7 +68,6 @@ def main():
             for _ in range(999):
                 y_trt += model_trt(x)
 
-
     print(torch.max(torch.abs(y - y_trt)))
     print(y)
     print(y_trt)
@@ -71,6 +81,17 @@ def eval_time(name: str, log_func=print):
     start = time.time()
     yield
     log_func(f"execution time of {name}: {time.time() - start: .6f}")
+
+
+def get_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Convert pytorch model to trt model and evaluate execution time of original and converted model"
+    )
+    parser.add_argument("original_model", type=Path)
+    parser.add_argument("trt_out", type=Path, default="trt_model")
+    parser.add_argument("-m", "--original_model_mode", type=str, default="pytorch", choices=['pytorch', 'trt'])
+
+    return parser
 
 
 if __name__ == '__main__':
